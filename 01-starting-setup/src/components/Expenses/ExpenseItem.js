dd611b379b5a47d2d1c7349645be6f93edfb58cf
Han1s <honzaranostaj@gmail.com>
2021-07-26 21:30:31 +0200

    48

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -9,6 +9,7 @@ const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
 
   const clickHandler = () => {
+    setTitle('Updated');
     console.log('Clicked!');
   }
 
@@ -16,7 +17,7 @@ const ExpenseItem = (props) => {
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}
         </div>
       </div>
