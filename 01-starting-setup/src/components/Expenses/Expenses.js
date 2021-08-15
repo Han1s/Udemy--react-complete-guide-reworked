346ffbf7ba1f7074b9c7830de481eef892170a5a
Jan Ranostaj <HonzaRanostaj@gmail.com>
2021-08-15 21:36:41 +0200

    Assignment 2

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,25 +1,38 @@
+import { Fragment, useState } from 'react';
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 
-const expenses = (props) => {
+const Expenses = (props) => {
+  const [year, setYear] = useState(2020);
+
   const expenses = props.expenses;
 
+  const yearChangeHandler = (event) => {
+    setYear(event.target.value);
+  }
+
   return (
-    <Card className='expenses'>
-      {
-        expenses.map((expense, index) => {
-          return (
-            <ExpenseItem
-              key={index} 
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date} />
-          )
-        })
-      }
-    </Card>
+    <Fragment>
+      <Card className='expenses'>\
+      <ExpensesFilter 
+        yearChangeHandler={yearChangeHandler}
+        year={year}/>
+        {
+          expenses.map((expense, index) => {
+            return (
+              <ExpenseItem
+                key={index} 
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date} />
+            )
+          })
+        }
+      </Card>
+    </Fragment>
   )
 }
 
-export default expenses;
+export default Expenses;
