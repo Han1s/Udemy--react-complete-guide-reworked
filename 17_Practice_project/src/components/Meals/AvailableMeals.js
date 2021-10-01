dbdfcafe79115fa909492771900ab4bc8a30d449
Jan Ranostaj <HonzaRanostaj@gmail.com>
2021-10-01 15:45:31 +0200

    214

--- 17_Practice_project/src/components/Meals/AvailableMeals.js
+++ 17_Practice_project/src/components/Meals/AvailableMeals.js
@@ -1,36 +1,33 @@
+import { useState, useEffect } from 'react';
+
 import classes from './AvailableMeals.module.css';
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
-
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    fetch('https://react-http-6a7a1-default-rtdb.europe-west1.firebasedatabase.app/meals.json')
+    .then(response => {
+      return response.json();
+    }).then(data => {
+      const loadedMeals = [];
+
+      for (const key in data) {
+        loadedMeals.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price
+        })
+      }
+
+      setMeals(loadedMeals);
+    });
+  }, [])
+
+  const mealsList = meals.map((meal) => (
     <MealItem 
       id={meal.id}
       key={meal.id}
