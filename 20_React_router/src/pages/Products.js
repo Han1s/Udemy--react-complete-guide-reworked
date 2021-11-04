30c0cb5414d5e03378e9e43c3af7ef0792dce1cc
Han1s <honzaranostaj@gmail.com>
2021-11-04 09:01:30 +0100

    273

--- 20_React_router/src/pages/Products.js
+++ 20_React_router/src/pages/Products.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 function Products(props) {
   return (
-    <h1>The Products Page</h1>
+    <Fragment>
+      <h1>The Products Page</h1>
+      <ul>
+        <li>A Book</li>
+        <li>A Carpet</li>
+        <li>An Online Course</li>
+      </ul>
+    </Fragment>
   );
 }
 
