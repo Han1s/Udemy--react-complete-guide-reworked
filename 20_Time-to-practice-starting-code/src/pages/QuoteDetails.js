f2bbf0e7cfcf8486d6d9c8fd4143111f1713e38e
Han1s <honzaranostaj@gmail.com>
2021-11-05 20:04:55 +0100

    281

--- 20_Time-to-practice-starting-code/src/pages/QuoteDetails.js
+++ 20_Time-to-practice-starting-code/src/pages/QuoteDetails.js
@@ -1,17 +1,30 @@
 import React, { Fragment } from 'react';
 import { useParams, Route } from 'react-router';
 import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
 
+const DUMMY_DATA = [
+  { id: 'q1', author: 'Max', text: 'Learning React is fun!'},
+  { id: 'q2', author: 'Maxmilian', text: 'Learning React is great!'},
+]
 
 
 const QuoteDetails = () => {
   const params = useParams();
 
+  const quote = DUMMY_DATA.find((quote) => {
+    return quote.id === params.quoteId;
+  })
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
   console.log(params.quoteId);
 
   return (
     <Fragment>
-      <h1>Quote Detail Page</h1>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
