28cfa3b9dc062e319d25ac61073333845cff8745
Han1s <honzaranostaj@gmail.com>
2021-11-05 19:34:34 +0100

    277

--- 20_Time-to-practice-starting-code/src/pages/QuoteDetails.js
+++ 20_Time-to-practice-starting-code/src/pages/QuoteDetails.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { useParams } from 'react-router';
 
 const QuoteDetails = () => {
+  const params = useParams();
+
+  console.log(params.quoteId);
+
   return (
     <h1>Quote Detail Page</h1>
   );
