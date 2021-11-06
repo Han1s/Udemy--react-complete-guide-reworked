61e1538d35623e7df66ca2a484e43414eb11110e
Han1s <honzaranostaj@gmail.com>
2021-11-06 10:21:42 +0100

    286

--- 20_Time-to-practice-starting-code/src/pages/QuoteDetails.js
+++ 20_Time-to-practice-starting-code/src/pages/QuoteDetails.js
@@ -1,13 +1,12 @@
 import React, { Fragment } from 'react';
-import { useParams, Route } from 'react-router';
+import { useParams, Route, Link } from 'react-router-dom';
 import Comments from '../components/comments/Comments';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
 
 const DUMMY_DATA = [
   { id: 'q1', author: 'Max', text: 'Learning React is fun!'},
   { id: 'q2', author: 'Maxmilian', text: 'Learning React is great!'},
-]
-
+];
 
 const QuoteDetails = () => {
   const params = useParams();
@@ -25,6 +24,11 @@ const QuoteDetails = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className='centered'>
+          <Link className='btn--flat' to={`/quotes/${params.quoteId}/comments`}>Load Comments</Link>
+        </div>
+      </Route>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
