e85e41691869843a3f4138d17295dd46ece81c20
Jan Ranostaj <HonzaRanostaj@gmail.com>
2021-11-23 12:09:27 +0100

    289

--- 20_Time-to-practice-starting-code/src/pages/AllQuotes.js
+++ 20_Time-to-practice-starting-code/src/pages/AllQuotes.js
@@ -1,14 +1,40 @@
+import { useEffect } from 'react';
 import React from 'react';
 import QuoteList from '../components/quotes/QuoteList';
-
-const DUMMY_DATA = [
-  { id: 'q1', author: 'Max', text: 'Learning React is fun!'},
-  { id: 'q2', author: 'Maxmilian', text: 'Learning React is great!'},
-]
+import useHttp from '../hooks/use-http';
+import {getAllQuotes} from '../lib/api';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+import NoQuotesFound from '../components/quotes/NoQuotesFound';
 
 const AllQuotes = () => {
+  const {sendRequest, status, data: loadedQuotes, error } = useHttp(getAllQuotes, true);
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  if (status === 'pending') {
+    return (
+    <div>
+      <LoadingSpinner />
+    </div>
+    )
+  }
+
+  if (error) {
+    return (
+      <p className='centered focused'>{error}</p>
+    )
+  }
+
+  if (status === 'completed' && (!loadedQuotes || loadedQuotes.length == 0)) {
+    return (
+      <NoQuotesFound />
+    )
+  }
+
   return (
-    <QuoteList quotes={DUMMY_DATA} />
+    <QuoteList quotes={loadedQuotes} />
   );
 };
 
--- 20_Time-to-practice-starting-code/src/pages/QuoteDetails.js
+++ 20_Time-to-practice-starting-code/src/pages/QuoteDetails.js
@@ -1,22 +1,40 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { useParams, Route, Link, useRouteMatch } from 'react-router-dom';
 import Comments from '../components/comments/Comments';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
-
-const DUMMY_DATA = [
-  { id: 'q1', author: 'Max', text: 'Learning React is fun!'},
-  { id: 'q2', author: 'Maxmilian', text: 'Learning React is great!'},
-];
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+import useHttp from '../hooks/use-http';
+import { getSingleQuote } from '../lib/api';
 
 const QuoteDetails = () => {
   const params = useParams();
   const match = useRouteMatch();
 
-  const quote = DUMMY_DATA.find((quote) => {
-    return quote.id === params.quoteId;
-  })
+  const { quoteId } = params;
 
-  if (!quote) {
+  const { sendRequest, status, data: loadedQuote, error  } = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === 'pending') {
+    return (
+      <div className='centered'>
+        <LoadingSpinner />
+      </div>
+    )
+  }
+
+  if (error) {
+    return (
+      <p classNam='centered'>
+        {error}
+      </p>
+    )
+  }
+
+  if (!loadedQuote.text) {
     return <p>No quote found!</p>;
   }
 
@@ -24,7 +42,7 @@ const QuoteDetails = () => {
 
   return (
     <Fragment>
-      <HighlightedQuote text={quote.text} author={quote.author} />
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
       <Route path={match.path} exact>
         <div className='centered'>
           <Link className='btn--flat' to={`${match.url}/comments`}>
